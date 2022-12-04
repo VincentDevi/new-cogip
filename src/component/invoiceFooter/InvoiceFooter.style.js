8975b6a3167310942b9f6df7d7818c4c638d9a7a
Devilers Vincent <vincent.devilers@gmail.com>
2022-12-04 17:59:07 +0100

    fix typo in invoiceFooterStyle

--- src/component/invoiceFooter/InvoiceFooter.style.js
+++ src/component/invoiceFooter/InvoiceFooter.style.js
@@ -1,17 +1,13 @@
 import styled from "styled-components";
 
 export const InvoiceFooterStyle = styled.div`
-      footer:{
         height:10vh;
         width:100vw;
         background-color:#1E2139;
         display:flex;
         align-items:center;
         justify-content:center;
-    }
     @media (min-width:640px){
-        footer{
             display:none;
-        }
     }
 `
