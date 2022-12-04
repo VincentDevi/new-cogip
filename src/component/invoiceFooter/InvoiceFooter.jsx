63cd24237d51ed8fa4eb26eaf8a8b0deb64add92
Devilers Vincent <vincent.devilers@gmail.com>
2022-12-04 19:12:37 +0100

    ligth mode in invoice footer

--- src/component/invoiceFooter/InvoiceFooter.jsx
+++ src/component/invoiceFooter/InvoiceFooter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { CudButton } from "../CudButton/CudButton";
 import { InvoiceFooterStyle } from "./InvoiceFooter.style";
+import { useSelector } from "react-redux";
 
 
 
 export const InvoiceFooter = () =>{
+    const mode = useSelector((state)=>state.darkMode.value)
     return (
-        <InvoiceFooterStyle>
+        <InvoiceFooterStyle mode={mode}>
             <CudButton/>
         </InvoiceFooterStyle>
     );
--- src/component/invoiceFooter/InvoiceFooter.style.js
+++ src/component/invoiceFooter/InvoiceFooter.style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const InvoiceFooterStyle = styled.div`
         height:10vh;
         width:100vw;
-        background-color:#1E2139;
+        background-color:${props=>(props.mode)? '#1E2139':'#FFFFFF'};
         display:flex;
         align-items:center;
         justify-content:center;
